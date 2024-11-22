fffb5c71965e1de3b3a6b18a2496143db577d22b
Cercelan Ariana <127732185+Mimsik@users.noreply.github.com>
2024-11-22 17:37:13 +0200

    Add files via upload

--- TVShowsPlatform/include/LimitedSeries.h
+++ TVShowsPlatform/include/LimitedSeries.h
@@ -6,10 +6,11 @@
 class LimitedSeries : public TvShow {
 private:
     int seasons;
+    int availableDays;  // Number of days available
 
 public:
     // Constructor
-    LimitedSeries(const std::string& title, const std::string& genre, int episodes, const char* desc, int seasons);
+    LimitedSeries(const std::string& title, const std::string& genre, int episodes, const char* desc, int seasons, int availableDays);
 
     // Copy Constructor
     LimitedSeries(const LimitedSeries& other);
@@ -17,8 +18,18 @@ public:
     // Move Constructor
     LimitedSeries(LimitedSeries&& other) noexcept;
 
+    // Copy Assignment Operator
+    LimitedSeries& operator=(const LimitedSeries& other);
+
+    // Move Assignment Operator
+    LimitedSeries& operator=(LimitedSeries&& other) noexcept;
+
     // Destructor
     ~LimitedSeries();
+
+    // Getter and days management
+    int getAvailableDays() const;
+    void decreaseAvailableDays();  // Decreases days by 1
 };
 
 #endif
